bb2ff839530309071773c1538486e3b9bb98a874
fcecin <fcecin@gmail.com>
2023-02-26 12:29:03 -0300

    first commit

---
+++ docs/intro.md
@@ -0,0 +1,14 @@
+---
+slug: /
+sidebar_position: 1
+---
+
+# Introduction
+
+This is the [UtilityX](https://utilityx.io) User Guide.
+
+This guide will show you how to to use all of UtilityX's features.
+
+> **_NOTE:_** This guide is written in a walkthrough format, but you can skip to any section that interests you by using the navigation bar on the left.
+
+Let's get started!
---
+++ docusaurus.config.js
@@ -0,0 +1,137 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'UtilityX User Guide',
+    tagline: 'UtilityX user guide.',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'CryptoMechanics', // Usually your GitHub org/user name.
+  projectName: 'utx-guide', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/', // Serve the docs at the site's root
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          //editUrl:
+          //  'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+          blog: false,/**{
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          //editUrl:
+          //  'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },*/
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'UtilityX User Guide',
+        logo: {
+          alt: 'UtilityX User Guide',
+          src: 'img/logo.svg',
+        },
+        items: [
+          /**{
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },*/
+          /**{to: '/blog', label: 'Blog', position: 'left'},*/
+          /**{
+            href: 'https://github.com/eosnetworkfoundation/ibc-docs',
+            label: 'GitHub',
+            position: 'right',
+          },*/
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          /**{
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },*/
+          {
+            title: 'Community',
+            items: [
+              /*{
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/eos-network',
+              },*/
+              {
+                label: 'Telegram',
+                href: 'https://t.me/UX_Network',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/network_ux',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: 'https://medium.com/@UX_Network',
+              },
+              /**{
+                label: 'GitHub',
+                href: 'https://github.com/eosnetworkfoundation/ibc-contracts',
+              },*/
+              {
+                label: '0rigin',
+                href: 'https://0rigin.one/',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} <a href="https://0rigin.one/">0rigin</a>`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
